56ec76948ec6b739e7692f072faf81b3f3ecd3e0
Harry Sadler <harrysadlermusic@gmail.com>
2018-03-30 23:58:05 -0700

    get redis and mysql connecting correctly to Flask server

--- server/app.py
+++ server/app.py
@@ -12,64 +12,60 @@ import redis
 app = Flask(__name__)
 
 # mysql instance and settings
-# mysql_con = mdb.connect(
-#     host='localhost',
-#     user='root',
-#     passwd='example',
-#     db='change_this...'
-# )
-# mysql_con.set_character_set('utf8')
-# mysql_cur = mysql_con.cursor(mdb.cursors.DictCursor)
-# mysql_cur.execute('SET NAMES utf8;')
-# mysql_cur.execute('SET CHARACTER SET utf8;')
-# mysql_cur.execute('SET character_set_connection=utf8;')
-
-# mysql usage example
-# with mysql_con:
-#     mysql_cur.execute("SELECT * FROM Writers LIMIT 4")
-#     rows = mysql_cur.fetchall()
-#     for row in rows:
-#         print(row["Id"], row["Name"])
-
+mysql_con = mdb.connect(
+    host='mysql',
+    user='root',
+    passwd='example',
+    db='test'
+)
+mysql_con.set_character_set('utf8')
+mysql_cur = mysql_con.cursor(mdb.cursors.DictCursor)
+mysql_cur.execute('SET NAMES utf8;')
+mysql_cur.execute('SET CHARACTER SET utf8;')
+mysql_cur.execute('SET character_set_connection=utf8;')
 
 # TODO: MongoDB instance and settings here...
 
 # redis instance
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(
+    host='redis',
+    port=6379
+    # password='example'
+)
 
 
 # METHODS
 
 def get_mysql_data():
-    # MOCK DATA
-    return {
-        'one': 1,
-        'two': 2
-    }
+    with mysql_con:
+        mysql_cur.execute("""
+            SELECT *
+            FROM test_table
+        """)
+        rows = mysql_cur.fetchall()
+        return rows
 
 def get_hit_cache_count():
-    # retries = 5
-    # while True:
-    try:
-        return cache.incr('hits')
-    except redis.exceptions.ConnectionError as exc:
-        return 'nothing'
-        # if retries == 0:
-            # raise exc
-        # retries -= 1
-        # time.sleep(0.5)
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 # ROUTES
 
-@app.route('/mysql')
-def mysql():
+@app.route('/get-mysql')
+def get_mysql():
     mysql_data = get_mysql_data()
     return 'Here\'s the datas:\n{}'.format(str(mysql_data))
 
-@app.route('/')
-def cache():
-    # return str(cache)
+@app.route('/get-cache')
+def get_cache():
     count = get_hit_cache_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
